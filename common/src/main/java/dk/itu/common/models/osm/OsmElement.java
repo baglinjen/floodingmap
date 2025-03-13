913735d2d6688d1e1a4e63a42a75b6fb8c87c241
Andreas <anbc@itu.dk>
2025-03-13 15:35:21 +0100

    Updated OsmElement.java

--- common/src/main/java/dk/itu/common/models/osm/OsmElement.java
+++ common/src/main/java/dk/itu/common/models/osm/OsmElement.java
@@ -5,13 +5,31 @@ import dk.itu.common.models.Geographical2D;
 
 import java.awt.*;
 
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.geom.MultiPolygon;
+
 public abstract class OsmElement extends Drawable implements Geographical2D {
     public long id;
+    protected Geometry geometry;
 
-    public OsmElement(long id) {
+    public OsmElement(long id, Geometry geometry) {
         this.id = id;
+        this.geometry = geometry;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Geometry getGeometry() {
+        return geometry;
     }
 
     public abstract double getArea();
-    public abstract Shape getShape();
+
+    public abstract boolean shouldBeDrawn();
 }
+
