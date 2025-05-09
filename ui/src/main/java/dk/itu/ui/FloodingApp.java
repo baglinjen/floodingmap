37d4df17e20fc42414c4f47116be1c014d605656
Marcus Aandahl <marcus.aandahl@gmail.com>
2025-05-09 12:18:02 +0200

    Improved gml latlon utm grid splitting

--- ui/src/main/java/dk/itu/ui/FloodingApp.java
+++ ui/src/main/java/dk/itu/ui/FloodingApp.java
@@ -39,7 +39,7 @@ public class FloodingApp extends GameApplication {
 
             // Temporary whilst using in-memory
 //            services.getOsmService(state.isWithDb()).loadOsmData("tuna.osm");
-            services.getOsmService(state.isWithDb()).loadOsmData("ky.osm");
+            services.getOsmService(state.isWithDb()).loadOsmData("bornholm.osm");
             state.resetWindowBounds();
             state.updateMinMaxWaterLevels(services);
 //            var bounds = state.getWindowBounds();
