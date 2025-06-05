93d266985b90eb60bd5657bddec166e02c3a4a3b
Marcus Aandahl <marcus.aandahl@gmail.com>
2025-06-05 18:27:26 +0200

    Added minor routing fixes with height curves

--- ui/src/main/java/dk/itu/ui/State.java
+++ ui/src/main/java/dk/itu/ui/State.java
@@ -181,7 +181,7 @@ public class State {
             var nodes = s.getOsmService(isWithDb()).getTraversableOsmNodes();
 
             //Distribute workload across N threads
-            int threadPool = Runtime.getRuntime().availableProcessors();
+            int threadPool = Runtime.getRuntime().availableProcessors() / 2; // Half to free up threads for other processes
             var workload = splitNodes(nodes, threadPool);
 
             List<Thread> threads = new ArrayList<>();
